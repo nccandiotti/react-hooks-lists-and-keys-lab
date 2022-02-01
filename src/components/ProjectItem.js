25c5f4d3d06247c28473207ad8c1dc59e791c325
Nicole Candiotti <nccandiotti@gmail.com>
2022-01-31 21:45:57 -0500

    commit tree b5870c68fb4a8747739b341e1b60fc7cba93bc03

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,6 +6,11 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+      {technologies.map((technology) => {
+        return ( <span key={technology}>{technology}</span> )
+      })}
+      
+        
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
